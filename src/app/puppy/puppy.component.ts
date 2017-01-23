9c8e736c2dcc98baa70b6235a46af7acc18ffc8d
Craig Nishina <craig.nishina@gmail.com>
2017-01-23 15:15:57 -0800

    tutorial: find the puppy name

--- src/app/puppy/puppy.component.ts
+++ src/app/puppy/puppy.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {AngularFire} from 'angularfire2';
+import {AngularFire, FirebaseObjectObservable} from 'angularfire2';
 
 import {Puppy} from './puppy.model';
 
@@ -11,6 +11,8 @@ export class PuppyComponent {
   puppy: Puppy = new Puppy();
 
   // TODO(you): add variables to lookup the puppy by name and the found puppy
+  lookup: string;
+  foundPuppy: FirebaseObjectObservable<Puppy>;
 
   // dependency injected angularfire
   constructor(public af: AngularFire) {}
@@ -18,11 +20,13 @@ export class PuppyComponent {
   save() {
     let observ = this.af.database.object('/puppy/' + this.puppy.name);
     observ.update(this.puppy);
-    console.log('save clicked, name entered: ', this.puppy.name);
+    console.log('saved on click.');
+    console.log('name entered: ', this.puppy.name);
+    console.log('owner entered: ', this.puppy.owner);
   }
 
   // TODO(you): find the puppy with af.database.get
   findPuppy() {
-
+    this.foundPuppy = this.af.database.object('/puppy/' + this.lookup);
   }
 }
