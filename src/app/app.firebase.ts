59626b73530157d57b6e5120deaf3969e48593b9
Craig Nishina <craig.nishina@gmail.com>
2017-01-23 13:12:11 -0800

    tutorial: add angularfire2 and firebase dependencies

---
+++ src/app/app.firebase.ts
@@ -0,0 +1,6 @@
+export const appFirebase = {
+  apiKey: 'AIzaSyDEM9kiz4faqLBbRl3DNNNxsw6siPPCuRk',
+  authDomain: 'angularhackday-puppy-noauth.firebaseapp.com',
+  databaseURL: 'https://angularhackday-puppy-noauth.firebaseio.com',
+  storageBucket: 'angularhackday-puppy-noauth.appspot.com'
+};
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {AngularFireModule} from 'angularfire2';
 
 import { AppComponent } from './app.component';
+import { appFirebase } from './app.firebase';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(appFirebase)
   ],
   providers: [],
   bootstrap: [AppComponent]
