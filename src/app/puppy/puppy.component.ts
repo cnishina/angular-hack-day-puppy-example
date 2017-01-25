7e433486b1e79b9213e852240c71eba2afc89b23
Craig Nishina <craig.nishina@gmail.com>
2017-01-24 19:28:11 -0800

    tutorial: TODO write and read lists

--- src/app/puppy/puppy.component.ts
+++ src/app/puppy/puppy.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {AngularFire, FirebaseObjectObservable} from 'angularfire2';
+import {AngularFire, FirebaseListObservable, FirebaseObjectObservable} from 'angularfire2';
 
 import {Puppy} from './puppy.model';
 
@@ -8,25 +8,42 @@ import {Puppy} from './puppy.model';
   templateUrl: './puppy.component.html',
 })
 export class PuppyComponent {
-  puppy: Puppy = new Puppy();
-
-  // TODO(you): add variables to lookup the puppy by name and the found puppy
-  lookup: string;
+  puppy: Puppy;
   foundPuppy: FirebaseObjectObservable<Puppy>;
+  foundPuppyPhotos: FirebaseListObservable<string[]>;
 
   // dependency injected angularfire
   constructor(public af: AngularFire) {}
 
-  save() {
-    let observ = this.af.database.object('/puppy/' + this.puppy.name);
-    observ.update(this.puppy);
+  save(name: string, owner: string, photo: string) {
+
+    // Add a puppy object
+    if (name || owner) {
+      let puppyObserv = this.af.database.object('/puppy/' + name);
+      puppyObserv.subscribe(puppySub => {
+        this.puppy = new Puppy();
+        this.puppy.name = name || puppySub.name;
+        this.puppy.owner = owner || puppySub.owner;
+        puppyObserv.update(this.puppy);
+      });
+    }
+
+
+    if (photo) {
+      let photoObserv = this.af.database.list('/puppy/' + name + '/photo');
+      photoObserv.subscribe(list => {
+        // TODO(you): Add a photo to the list, if the item does not exist
+        // push them onto the list then update
+      });
+    }
+
     console.log('saved on click.');
-    console.log('name entered: ', this.puppy.name);
-    console.log('owner entered: ', this.puppy.owner);
   }
 
-  // TODO(you): find the puppy with af.database.get
-  findPuppy() {
-    this.foundPuppy = this.af.database.object('/puppy/' + this.lookup);
+
+  findPuppy(lookup: string) {
+    console.log(lookup);
+    this.foundPuppy = this.af.database.object('/puppy/' + lookup);
+    // TODO(you): get an observable list 'findPuppyPhotos' with af.database.list
   }
 }
